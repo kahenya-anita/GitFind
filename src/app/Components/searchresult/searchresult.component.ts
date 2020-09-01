e05c7322ae13aebcc24df26f95e18e5d65ec682e
kahenya-anita <anitakahenya1@gmail.com>
2020-09-01 21:45:20 +0300

    Update searchResult component

--- src/app/Components/searchresult/searchresult.component.ts
+++ src/app/Components/searchresult/searchresult.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Users } from '../../Model/users';
+import { RepositoriesByName } from '../../Model/repositories-by-name';
+import { Repositories } from '../../Model/repositories'; 
+import { SearchGitService } from 'src/app/services/search-git';
 
 @Component({
   selector: 'app-searchresult',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchresultComponent implements OnInit {
 
-  constructor() { }
+  repos:Repositories[];
+  user:Users;
+  username:string;  
 
-  ngOnInit(): void {
+  constructor( private route: ActivatedRoute, private searchGitService: SearchGitService ) {}
+
+  searchResult(){
+    this.username = this.route.snapshot.paramMap.get('username')
+    this.searchGitService.userInfoRequest(this.username)
+    this.user = this.searchGitService.user
+    this.searchGitService.userRepoRequest(this.username)
+    this.repos =this.searchGitService.repos
   }
 
+  ngOnInit(){
+    this.searchResult()
+  }
 }
