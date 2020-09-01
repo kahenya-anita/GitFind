59742dff40c8745c815be9ab1b2cf758667febbb
kahenya-anita <anitakahenya1@gmail.com>
2020-09-01 21:52:12 +0300

    Add accesstoken

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,9 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  gitUrl: "https://api.github.com/users/kahenya-anita",
+  giturl: "https://api.github.com/users",
+  API_Key: "2b38d2c3640e8f90d3673e366ec45989d8ab844a8",
+  accesstoken: "?access_token=2b38d2c3640e8f90d3673e366ec45989d8ab844a8"
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
