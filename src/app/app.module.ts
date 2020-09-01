6569536a6e77bc533281e19bb4f8ab63ec4e88b2
kahenya-anita <anitakahenya1@gmail.com>
2020-09-01 21:28:18 +0300

    Add more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { RepoSearchComponent } from './Components/repo-search/repo-search.component';
+import { SearchresultComponent } from './Components/searchresult/searchresult.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavBarComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ProfileComponent,
+    RepoSearchComponent,
+    SearchresultComponent
   ],
   imports: [
     BrowserModule,
